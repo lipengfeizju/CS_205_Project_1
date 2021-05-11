74c78d3fbddb9610de49c5b2c7b800d04acb94c2
Pengfei Li <lipengfei@lipengfeis-MacBook-Pro.local>
2021-05-11 15:11:46 -0600

    add comments and readme doc

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CS_205_Project_1
+
+New implementation ahcieves about 4x speed up for Depth=24 solution, in naive implementation, the node stores all the previous states up to now, it is time consuming to store and compare. In this new implementation, each node only stores the current state and the most recent action, and it also connects to its parent. When you want to know the whole chain up to now, you can traverse the tree up to the root.
--- main.py
+++ main.py
@@ -3,28 +3,39 @@ import heapq
 import copy 
 import timeit
 from utils import expand_node, hash_state, Tree
+from utils import misplaced_tile_dist
 
 def general_search(init_state, method):
     assert method in {0,1,2}
-    init_node = Tree(init_state,method)
+    
+    # The queue for nodes is implemented by queue
     node_heap = []
+    # Track the explored states to avoid repeated states
     explored_states = set()
+    # Push the initial node to the queue 
+    init_node = Tree(init_state,method)
     heapq.heappush(node_heap, init_node)
     explored_states.add(hash_state(init_state))
+    # Save the number of expanded nodes and maximum length of queue
     i = 0
     max_queue_len = 0
     while(1):
-
+        i += 1
         if len(node_heap) > max_queue_len: max_queue_len = len(node_heap)
         
         if len(node_heap) == 0:
             print("FAILURE!")
             return -1
+        # Pop up the node with mimimum cost
         min_node = heapq.heappop(node_heap)
+        # Print out details about this node
         print("g(n) = {} , h(n) = {}, f(n) = {}".format(min_node.g_n, min_node.h_n, min_node.g_n + min_node.h_n))
-        if min_node.h_n == 0:
+
+        # Judge if all numbers are in the correct position
+        if misplaced_tile_dist(min_node.state) == 0:
             print(i)
             print("FOUND!")
+            # Track all the actions and history states taken so far 
             action_list = []
             state_list = []
             while min_node.parent != None:
@@ -32,18 +43,23 @@ def general_search(init_state, method):
                 state_list.append(min_node.state)
                 min_node = min_node.parent
             state_list.append(min_node.state)
+            # This visit is from goal to initial state, we need to reverse it
             action_list.reverse()
             state_list.reverse()
             return action_list, state_list
 
+        # Find all the possible next movements based on current position
         node_list = expand_node(min_node)
         for node in node_list:
+            # For each move, we check if it is already visited before
             node_hash = hash_state(node.state)
             if node_hash not in explored_states:
+                # If this is a new state, add the node to queue and update
+                # explored states
                 explored_states.add(node_hash)
                 heapq.heappush(node_heap, node)
 
-        i += 1
+        
     
 if __name__ == "__main__":
     # init_state = np.array([1,3,6,5,0,2,4,8,7]).reshape([3,3])
@@ -61,7 +77,7 @@ if __name__ == "__main__":
     # init_state = np.array([1,2,3,4,5,6,0,7,8]).reshape([3,3]) # 2
 
     # print(hash_state(init_state))
-    action_res, state_res  = general_search(init_state,2)
+    action_res, state_res  = general_search(init_state,0)
     print(action_res)
 
     stop = timeit.default_timer()
